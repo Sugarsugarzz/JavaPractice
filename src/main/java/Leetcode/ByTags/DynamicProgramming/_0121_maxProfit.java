023f2a1600ac9b68482c5baab038945127cfa5e6
Sugarbabyzz <406857586@qq.com>
2020-08-09 23:42:44 +0800

    Create _0121_maxProfit.java

---
+++ src/main/java/Leetcode/ByTags/DynamicProgramming/_0121_maxProfit.java
@@ -0,0 +1,22 @@
+package Leetcode.ByTags.DynamicProgramming;
+
+/**
+ * 121. 买卖股票的最佳时机
+ * Easy
+ */
+
+public class _0121_maxProfit {
+
+    public static void main(String[] args) {
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        
+
+        return 0;
+    }
+}
