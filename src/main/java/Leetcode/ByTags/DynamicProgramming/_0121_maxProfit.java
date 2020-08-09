21b5f7f4eed6a3d17c62472e49430848834da7e4
Sugarbabyzz <406857586@qq.com>
2020-08-10 00:17:00 +0800

    Day47 Leetcode

--- src/main/java/Leetcode/ByTags/DynamicProgramming/_0121_maxProfit.java
+++ src/main/java/Leetcode/ByTags/DynamicProgramming/_0121_maxProfit.java
@@ -15,8 +15,25 @@ public class _0121_maxProfit {
 
     public static int maxProfit(int[] prices) {
 
-        
+        // 一、双指针（暴力法）
+//        int max = 0;
+//        for (int i = 0; i < prices.length; i++)
+//            for (int j = i + 1; j < prices.length; j++) {
+//                max = Math.max(prices[j] - prices[i], max);
+//            }
+//        return max;
 
-        return 0;
+        // 二、动态规划
+        // 维护两个变量，买入价格 和 利润
+        if (prices.length == 0)
+            return 0;
+        int buy = prices[0], profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (buy >= prices[i])
+                buy = prices[i];
+            else if (prices[i] - buy > profit)
+                profit = prices[i] - buy;
+        }
+        return profit;
     }
 }
