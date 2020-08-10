3fe8f1ea9df35397404a39477743aa60c257be93
Sugarbabyzz <406857586@qq.com>
2020-08-10 11:07:12 +0800

    Day48 Leetcode

--- src/main/java/Leetcode/ByTags/DynamicProgramming/_0070_climbStairs.java
+++ src/main/java/Leetcode/ByTags/DynamicProgramming/_0070_climbStairs.java
@@ -9,28 +9,29 @@ public class _0070_climbStairs {
 
     public static void main(String[] args) {
 
-        System.out.println(climbStairs(3));
+        System.out.println(climbStairs(10));
     }
 
     public static int climbStairs(int n) {
 
         // 最基本的动态规划
-//        int[] dp = new int[n + 1];
+        int[] dp = new int[n + 1];
 //        dp[0] = 1;
-//        dp[1] = 1;
-//        for (int i = 2; i <= n; i++) {
-//            dp[i] = dp[i - 1] + dp[i - 2];
-//        }
-//        return dp[n];
+        dp[1] = 1;
+        dp[2] = 2;  // 设定 2 级 的为初始值更好理解些
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
 
         // 空间压缩（只存储 i-1 和 i-2 的值即可）
-        int a = 1, b = 1, t = 0;
-
-        for (int i = 2; i <= n; i++) {
-            t = b;
-            b = t + a;
-            a = t;
-        }
-        return  b;
+//        int a = 1, b = 1, t = 0;
+//
+//        for (int i = 2; i <= n; i++) {
+//            t = b;
+//            b = t + a;
+//            a = t;
+//        }
+//        return  b;
     }
 }
