14cd510957da637882ffdad4b28f02ae5d5b0735
sugar <406857586@qq.com>
2021-01-12 14:24:36 +0800

    Leetcode 322

---
+++ Basic-Learn/src/main/java/Leetcode/ByTags/DynamicProgramming/_0322_coinChange.java
@@ -0,0 +1,36 @@
+package Leetcode.ByTags.DynamicProgramming;
+
+import java.util.Arrays;
+
+/**
+ * 322. 零钱兑换
+ * Medium
+ */
+
+public class _0322_coinChange {
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.println(coinChange(coins, amount));
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+
+        // 一、动态规划
+        if (amount == 0)  return 0;
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
